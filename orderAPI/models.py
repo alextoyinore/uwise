708efa5cf00161069101b811251681f77ee1e3c2
Alexander Ore <aore8030@gmail.com>
2024-02-14 13:13:39 +0100

    few ui touchups

--- orderAPI/models.py
+++ orderAPI/models.py
@@ -25,10 +25,13 @@ class Cart(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order_user', null=False, blank=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='order_course', null=False, blank=False)
+    transaction_id = models.CharField(max_length=200, null=False, blank=False)
+    transaction_reference = models.CharField(max_length=200, null=True, blank=True)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('user', 'course')
+        unique_together = ('user', 'course', 'transaction_id', 'transaction_reference')
         ordering = ('date_added',)
         verbose_name_plural = 'Orders'
         get_latest_by = 'date_added'
@@ -36,3 +39,20 @@ class Order(models.Model):
 
     def __str__(self):
         return f'{self.user.get_full_name()} - {self.course.title}'
+
+
+class UserPurchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchase_by_user', null=False, blank=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='purchased_course', null=False, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'course')
+        ordering = ('date_added',)
+        verbose_name_plural = 'UserPurchases'
+        get_latest_by = 'date_added'
+        verbose_name = 'UserPurchase'
+
+    def __str__(self):
+        return f'{self.user.get_full_name()} - {self.course.title}'
+    
