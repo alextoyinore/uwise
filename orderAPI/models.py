5d39f032f991396ab6853d718d2f1da628b4c6aa
Alexander Ore <aore8030@gmail.com>
2024-03-15 11:02:29 +0100

    made transaction id nullable

--- orderAPI/models.py
+++ orderAPI/models.py
@@ -25,7 +25,7 @@ class Cart(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order_user', null=False, blank=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='order_course', null=False, blank=False)
-    transaction_id = models.CharField(max_length=200, null=False, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True, blank=True)
     transaction_reference = models.CharField(max_length=200, null=True, blank=True)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateTimeField(auto_now_add=True)
